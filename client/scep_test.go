923efde12625594f315e5ced45f5cc3faa28686c
Victor Vrantchan <vrancean+github@gmail.com>
2016-08-11 21:34:15 -0400

    add support for encoding the request in url params and  use GET if POST is not supported

--- client/scep_test.go
+++ client/scep_test.go
@@ -32,3 +32,51 @@ func TestGetCACaps(t *testing.T) {
 	}
 	fmt.Println(string(resp))
 }
+
+func TestGetCACert(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		dump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println(string(dump))
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+	client := NewClient(server.URL + "/scep")
+	ctx := context.Background()
+
+	resp, _, err := client.GetCACert(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(resp))
+}
+
+func TestPKIOperationGET(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		dump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println(string(dump))
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+	client := NewClient(server.URL + "/scep")
+	ctx := context.Background()
+
+	resp, err := client.PKIOperation(ctx, []byte("foobarbaz"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(resp))
+}
